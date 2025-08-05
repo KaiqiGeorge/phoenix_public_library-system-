253c0de461843a3b7e4fdba35688de0d87464c24
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2025-08-04 22:24:51 -0700

    import register page path to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import EventDetail from "./pages/components/EventDetail";
+import Register from "./pages/Register";
 
 //设置路由
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
         <Route path="/events/:id" element={<EventDetail />} />
+        <Route path="/signup" element={<Register />} /> 
       </Routes>
     </Router>
   );
